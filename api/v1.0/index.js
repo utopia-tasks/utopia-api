543ca77c99469970c0d37b4bad7db0577c45466e
Kartik Sawant <kartik.sawant@oracle.com>
2020-06-21 23:26:10 -0700

    added create user endpoint

--- api/v1.0/index.js
+++ api/v1.0/index.js
@@ -1,5 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express'),
+	router = express.Router(),
+	{Pool} = require('pg'),
+	{v4: uuidv4} = require('uuid');
+
+const pool = new Pool();
+
+router.post('/sign-up', function (req, res) {
+	pool.connect((err, client, release) => {
+		if (err) {
+			res.status(404).send('User cannot be made');
+			return console.error('Error acquiring client', err.stack);
+		}
+		const query = 'SELECT * FROM createAccount($1::text, $2, $3, $4, $5, $6, $7, $8)';
+		const guid = uuidv4();
+		const vars = [guid, req.body.username, req.body.password, req.body.firstName, req.body.lastName, req.body.email, req.body.language, req.body.offsetHours];
+		console.log('Creating user with: ' + query);
+		client.query(query, vars, (err, result) => {
+			release();
+			if (err) {
+				res.status(404).send('User cannot be made');
+				return console.error('Error executing query', err.stack)
+			}
+			res.send(result.rows);
+		})
+	})
+});
+
+router.get('/user?:id', function (req, res) {
+	pool.connect((err, client, release) => {
+		if (err) {
+			return console.error('Error acquiring client', err.stack);
+		}
+		console.log(req.cookies);
+		const query = 'SELECT * FROM user_info.user_details WHERE user_guid = $1::text';
+		console.log('Running query: ' + query);
+		client.query(query, [req.query.id], (err, result) => {
+			release();
+			if (err) {
+				res.send('User not found');
+				return console.error('Error executing query', err.stack)
+			}
+			res.send(result.rows);
+		})
+	})
+});
 
 router.get('/ping', function (req, res) {
 	console.log(req.headers);
--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express'),
 	app = express(),
 	cors = require('cors'),
-	cookieParser = require('cookie-parser');
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	https = require('https'),
+	fs = require('fs');
 const port = 3000;
+const options = {
+	key: fs.readFileSync('sslcert/key.pem'),
+	cert: fs.readFileSync('sslcert/cert.pem')
+};
 
 app.use(cors());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use('/api', require('./api'));
+const server = https.createServer(options, app);
 
-app.listen(port, function () {
-	console.log(`Utopia NodeJS Server listening at http://localhost:${port}`);
+server.listen(port, function () {
+	console.log(`Utopia NodeJS Server listening at https://localhost:${port}`);
 });
